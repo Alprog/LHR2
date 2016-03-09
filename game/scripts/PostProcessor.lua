3102690f0011566087fa7cd74bdba52add2c9a42
Alexander Tuzhik <alprog@hotmail.com>
2016-03-09 05:39:18 +0200

    render refactor

--- game/scripts/PostProcessor.lua
+++ game/scripts/PostProcessor.lua
@@ -15,48 +15,6 @@ function PostProcessor:init(app)
     --self.frameBuffer:retain()
 end
 
-function PostProcessor:setup(gBuffer, shadowMapBuffer, outBuffer, sceneCamera, lightCamera)
-    
-    self.frameBuffer = outBuffer
-    
-    local shader = getShader('sprite', 'halflambert')
-    local state = cc.GLProgramState:create(shader)
-    
-    --[[local texelSize = Vec(1 / self.frameBuffer.width, 1 / self.frameBuffer.height)    
-    state:setUniformVec2('texelSize', texelSize)
-    local scale = math.min(0.33 / theApp:getDeltaTime(), 1)
-    state:setUniformFloat('velocityScale', scale)]]
-    
-    local position = lightCamera:getPosition3D()  
-    state:setUniformVec3('lightPosition', position)
-    
-    state:setUniformTexture('albedoTexture', gBuffer:getAlbedoTexture())
-    state:setUniformTexture('normalTexture', gBuffer:getNormalTexture())
-    state:setUniformTexture('depthTexture', gBuffer:getDepthStencilTexture())
-    --state:setUniformTexture('velocityTexture', gBuffer:getRenderTarget(3))
-    
-    state:setUniformTexture('shadowMapTexture', shadowMapBuffer:getDepthStencilTexture())
-    
-    self.sprite:setGLProgramState(state)
-    
-    
-    state:setUniformTexture('normalTexture', gBuffer:getRenderTarget(1))
-    state:setUniformTexture('wrapTexture', getTexture('wrap2.png'))
-    
-    
-    local screenToWorld = cc.mat4.getInversed(sceneCamera:getViewProjectionMatrix())
-    state:setUniformMat4('screenToWorld', screenToWorld)
-    
-    if lightCamera then
-        local worldToShadowMap = lightCamera:getViewProjectionMatrix()
-        state:setUniformMat4('worldToShadowMap', worldToShadowMap)
-    end
-    
-    --local rt = gBuffer:getRenderTarget(0)
-    --self.frameBuffer:attachRenderTarget(rt, i - 1)
-end
-
-
 function PostProcessor:perform()
     self.camera:render(self.sprite, cc.CameraFlag.DEFAULT, self.frameBuffer)
 end
--- game/scripts/World3D.lua
+++ game/scripts/World3D.lua
@@ -2,34 +2,53 @@
 World3D = Derive("World3D", cc.Node)
 
 function World3D:init()
-    self.shadowMapBuffer = FrameBuffer:create(self, cc.size(1024, 1024), function(sender, size)
-        sender:attachNewTexture(FrameBuffer.Index.DepthStencil, size, cc.DEPTH24_STENCIL8)
-    end)
+    
+    self.gBuffer = ccexp.FrameBuffer:create(1)
+    
+    
+    self:keepObject(self.gBuffer)
+    
+    
+    self:initShadowMap()
         
-    self.gBuffer = GBuffer:create(self, theApp.windowSize)
     self.frameBuffer = FrameBuffer:create(self, theApp.windowSize, function(sender, size)
         sender:attachNewTexture(0, size, cc.BGRA8888)
     end) 
 end
 
-function World3D:createTexture(size, format)
-    return cc.Texture2D:create(size.width, size.height, format or cc.BGRA8888)
+function World3D:initShadowMap()
+    local size = cc.size(1024, 1024)
+    self.shadowMapBuffer = ccexp.FrameBuffer:create(1)
+    self.shadowMapBuffer:setSize(size.width, size.height)
+    self.shadowMapTexture = self:createTexture(size, cc.DEPTH24_STENCIL8)
+    self.shadowMapBuffer:attachDepthStencil(self.shadowMapTexture)
+    self:keepObject(self.shadowMapBuffer)
 end
 
-function World3D:initTextures(size)
-    self.albedoTexture = createTexture(size)
-    self.normalTexture = createTexture(size)
-    self.idsTexture = createTexture(size)
-    self.velocityTexture = createTexture(size, cc.RG16F)
-    self.depthStencil = createTexture(size, cc.DEPTH24_STENCIL8)
+function World3D:createTexture(size, format)
+    return cc.Texture2D:create(size.width, size.height, format or cc.BGRA8888, 0)
+end
+
+function World3D:initTextures(size)   
     
-    self.shadowMapTexture = createTexture(cc.size(1024, 1024), cc.DEPTH24_STENCIL8)
-    self.historyTexture = createTexture(size)
+    self.historyTexture = self:createTexture(size)
 end
 
 function World3D:onResize(size)
+    self.albedoTexture = self:createTexture(size)
+    self.normalTexture = self:createTexture(size)
+    self.idsTexture = self:createTexture(size)
+    self.velocityTexture = self:createTexture(size, cc.RG16F)
+    self.depthTexture = self:createTexture(size, cc.DEPTH24_STENCIL8)
+    
+    self.gBuffer:setSize(size.width, size.height)
+    self.gBuffer:attachRenderTarget(0, self.albedoTexture)
+    self.gBuffer:attachRenderTarget(1, self.normalTexture)
+    self.gBuffer:attachRenderTarget(2, self.idsTexture)
+    self.gBuffer:attachRenderTarget(3, self.velocityTexture)
+    self.gBuffer:attachDepthStencil(self.depthTexture)
+    
     self:setTransformUpdated()
-    self.gBuffer:resize(size)
     self.frameBuffer:resize(size)
     self.camera:setWindowAspect()
 end
@@ -52,14 +71,45 @@ function World3D:bakeShadows()
     if self.lightCamera then
         self.lightCamera:render(self, cc.CameraFlag.DEFAULT, self.shadowMapBuffer)
     end
-
-    self.frameBuffer:clearFBO()
-    thePostProcessor:setup(self.gBuffer, self.shadowMapBuffer, self.frameBuffer, self.camera, self.lightCamera)
-    thePostProcessor:perform()
 end
 
 function World3D:lighting()
+    self.frameBuffer:clearFBO()
     
+    
+    --thePostProcessor:setup(self.gBuffer, self.shadowMapBuffer, self.frameBuffer, self.camera, self.lightCamera)
+     
+     
+    thePostProcessor.frameBuffer = self.frameBuffer
+     
+    local shader = getShader('sprite', 'halflambert')
+    local state = cc.GLProgramState:create(shader)
+
+    
+    local position = self.lightCamera:getPosition3D()  
+    state:setUniformVec3('lightPosition', position)
+    state:setUniformTexture('albedoTexture', self.albedoTexture)
+    state:setUniformTexture('normalTexture', self.normalTexture)
+    state:setUniformTexture('depthTexture', self.depthTexture)
+    state:setUniformTexture('shadowMapTexture', self.shadowMapTexture)
+    state:setUniformTexture('wrapTexture', getTexture('wrap2.png'))
+    
+    local screenToWorld = cc.mat4.getInversed(self.camera:getViewProjectionMatrix())
+    state:setUniformMat4('screenToWorld', screenToWorld)
+    
+    if self.lightCamera then
+        local worldToShadowMap = self.lightCamera:getViewProjectionMatrix()
+        state:setUniformMat4('worldToShadowMap', worldToShadowMap)
+    end
+    
+    thePostProcessor.sprite:setGLProgramState(state)
+    thePostProcessor:perform()
+    
+        
+    --[[local texelSize = Vec(1 / self.frameBuffer.width, 1 / self.frameBuffer.height)    
+    state:setUniformVec2('texelSize', texelSize)
+    local scale = math.min(0.33 / theApp:getDeltaTime(), 1)
+    state:setUniformFloat('velocityScale', scale)]]
 end
 
 function World3D:renderTranparent()
